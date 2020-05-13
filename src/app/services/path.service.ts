34a25edf68f911c98cd8833130da9bd6504cb287
offline <felix.ruttan@gmail.com>
2020-05-13 15:26:51 -0700

    fixed Local

--- src/app/services/path.service.ts
+++ src/app/services/path.service.ts
@@ -2,13 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable() 
 export class pathService {
-<<<<<<< HEAD
-  //path: string = "http://localhost:1337/";
-  path: string = "/";
-=======
   path: string = "http://localhost:3000/";
   //path: string = "/";
->>>>>>> master
 
   constructor() { }
 }
